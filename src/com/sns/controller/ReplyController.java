75e6a9eb284f66cc403d8dde6e650aac8b7a6a2a
Heewon <gmldnjs0402@naver.com>
2021-03-17 15:17:29 +0900

    댓글 Controller, Service추가

---
+++ src/com/sns/controller/ReplyController.java
@@ -0,0 +1,51 @@
+package com.sns.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sns.service.ReplyService;
+
+@WebServlet({"/rlist","/rwrite","/rdel","/replyForm"})
+public class ReplyControll extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dual(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dual(req, resp);
+	}
+
+	private void dual(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");//한글깨짐 방지
+		
+		String uri = req.getRequestURI();
+		String ctx = req.getContextPath();
+		String subAddr = uri.substring(ctx.length());
+		
+		ReplyService reply = new ReplyService(req,resp);
+		
+		switch(subAddr) {
+		case "/rlist":
+			System.out.println("댓글리스트 요청");
+			reply.rlist();
+			break;
+		case "/rwrite":
+			System.out.println("댓글쓰기 요청");
+			reply.rwrite();
+			break;
+		
+		case "/rdel":
+			System.out.println("댓글삭제 요청");
+			reply.rdel();
+			break;
+		}
+	}
+}
---
+++ src/com/sns/service/ReplyService.java
@@ -0,0 +1,12 @@
+package com.sns.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReplyService {
+
+	public ReplyService(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
