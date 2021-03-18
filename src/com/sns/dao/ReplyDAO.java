cd7db9563e19603b9774b9c948d542c307113ef4
DESKTOP-FHGO7O0\gmldn <gmldnjs0402@naver.com>
2021-03-18 11:05:45 +0900

    댓글 작성,삭제,리스트 서비스, 게시판 파일 UI변경

--- src/com/sns/dao/ReplyDAO.java
+++ src/com/sns/dao/ReplyDAO.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.sns.dto.ReplyDTO;
+
 
 public class ReplyDAO {
 	Connection conn = null;
@@ -32,6 +36,85 @@ public class ReplyDAO {
 		}catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	public boolean del(String cmt_idx) {
+		boolean success = false;
+		String sql = "DELETE FROM Reply2 WHERE cmt_idx=?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cmt_idx);
+			if(ps.executeUpdate()>0){
+				success=true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+		return success;
+	}
+
+	public ArrayList<ReplyDTO> list(String user_id, String board_idx) {
+		String sql = "SELECT cmt_idx,user_id,board_idx,content FROM Reply2 WHERE user_id=? AND board_idx=? ORDER BY cmt_idx DESC";
+		
+		ArrayList<ReplyDTO> list = new ArrayList<ReplyDTO>();
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, user_id);
+			ps.setString(2, board_idx);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				ReplyDTO dto = new ReplyDTO();
+				dto.setCmt_idx(rs.getInt("cmt_idx"));
+				dto.setBoard_idx(rs.getInt("board_idx"));
+				dto.setUser_id(rs.getString("user_id"));
+				dto.setContent(rs.getString("content"));
+				list.add(dto);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+		return list;
+	}
+
+	public boolean write(String board_idx, String user_id, String content) {
+		boolean success = false;
+		String sql = "INSERT INTO reply2(cmt_idx,user_id,board_idx,content) VALUES(reply2_seq.NEXTVAL,?,?,?)";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, user_id);
+			ps.setString(2, board_idx);
+			ps.setString(3, content);
+			if(ps.executeUpdate()>0){
+				success=true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+		return success;
+	}
+
+	public int rcnt(String board_idx ) {
+		String sql = "SELECT COUNT(*) AS rcnt FROM Reply2 WHERE board_idx=?";
+		int success = 0;
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, board_idx);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				
+				success = rs.getInt("rcnt");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+		return success;
 	}
 	
 	
